7b0bf7fb774d6fe0a71241ff655524e14f531c33
Taavetl <163862539+Taavetl@users.noreply.github.com>
2024-04-22 17:24:24 +0300

    Add files via upload

---
+++ Arrays/Program.cs
@@ -0,0 +1,8 @@
+﻿string[] snacks = { "sushi", "pizza", "pizza", "burger", "chicken" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"me votame {snacks[randomIndex]}.");
+
---
+++ randomarray/Program.cs
@@ -0,0 +1,22 @@
+﻿string[] fruit = new string[3];
+
+fruit[0] = "apples";
+fruit[1] = "bananas";
+fruit[2] = "oranges";
+
+Console.WriteLine($" {fruit[0]}, {fruit[1]}, {fruit[2]}");
+
+int fruitarraylenght = fruit.Length;
+
+Console.WriteLine($"there are{fruitarraylenght} in your array.");
+
+for (int i = 0; i < fruitarraylenght; i++)
+{
+    fruit[i] = fruit[i].Replace(fruit[i][0], char.ToUpper(fruit[i][0]));
+
+}
+foreach (string element in fruit)
+{
+    Console.WriteLine(element);
+
+}
